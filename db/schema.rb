fed0ac9633693bd1cb03d86b4862e4f05b079902
rennrennrenn <rene@railslove.com>
2017-11-07 09:58:23 +0100

    database.yml is deletet from git instead example is added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103165929) do
+ActiveRecord::Schema.define(version: 20171106155329) do
+
+  create_table "expenses", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", limit: 40
