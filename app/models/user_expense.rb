82796ede64b62a6b02ce14059d52f8b6cdb1395c
rennrennrenn <rene@railslove.com>
2017-11-09 11:20:38 +0100

    Deleted User_Expense_Joint

--- app/models/user_expense.rb
+++
@@ -1,4 +0,0 @@
-class UserExpense < ApplicationRecord
-  belongs_to :expense
-  has_one :user
-end
--- db/migrate/20171107142609_create_user_expenses_join.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserExpensesJoin < ActiveRecord::Migration[5.1]
-  def change
-    create_table :user_expenses do |t|
-      t.integer :expense_id
-      t.integer :user_id
-    end
-    add_index('user_expenses', %w[expense_id user_id])
-  end
-end
