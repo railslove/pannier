fd1cc3176d7a0611fa4e09874d8144bddc5bdae3
rennrennrenn <rene@railslove.com>
2017-11-09 12:24:06 +0100

    Createt Migation for Bill with user_id, expense_id and amount

---
+++ db/migrate/20171109111106_create_bill.rb
@@ -0,0 +1,11 @@
+class CreateBill < ActiveRecord::Migration[5.1]
+  def change
+    create_table :bills do |t|
+      t.integer :user_id
+      t.integer :expense_id
+      t.decimal :amount, precision: 6, :scale: 2
+
+      add_index :user_expenses, %w[expense_id user_id], unique: true
+    end
+  end
+end
