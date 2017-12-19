1a683f01cff2bba030b1fd8012bd9514f364136b
rennrennrenn <rene@railslove.com>
2017-12-19 12:40:41 +0100

    Changed billing in expense.rb

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,24 +2,26 @@ class Expense < ApplicationRecord
   belongs_to :user
   has_many :bills
 
-  attr_accessor :amount
+  def amount=(value)
+    @amount = value.tr(',', '.').to_f * 100.to_i
+  end
+
+  def amount
+    Money.new(@amount, 'EUR')
+  end
+
   attr_accessor :user_ids
   attr_accessor :include_current_user_in_bill
 
   def create_bills
-    amount_in_money = Money.new(amount.tr(',', '.').to_f * 100.to_i, 'EUR')
-    create_bills_split(amount_in_money)
-  end
-
-  def create_bills_split(amount)
     if include_current_user_in_bill == '1'
-      create_bills_split_with_current_user(amount)
+      create_bills_split_with_current_user
     else
-      create_bills_split_without_current_user(amount)
+      create_bills_split_without_current_user
     end
   end
 
-  def create_bills_split_with_current_user(amount)
+  def create_bills_split_with_current_user
     splitted_amount = amount.split(user_ids.count + 1)
     create_bill(user_id, amount - splitted_amount.pop)
     user_ids.each do |user_id|
@@ -27,7 +29,7 @@ class Expense < ApplicationRecord
     end
   end
 
-  def create_bills_split_without_current_user(amount)
+  def create_bills_split_without_current_user
     splitted_amount = amount.split(user_ids.count)
     create_bill(user_id, amount)
     user_ids.each do |user_id|
@@ -35,7 +37,7 @@ class Expense < ApplicationRecord
     end
   end
 
-  def create_bill(user_id, amount)
-    bills.create(user_id: user_id, amount: amount)
+  def create_bill(user_id, bill_amount)
+    bills.create(user_id: user_id, amount: bill_amount)
   end
 end
